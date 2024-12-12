2664a17703a8b7b64d09e0ac77f1628161539a0f
Yushiro Kishimoto <yushiro.kishimoto@gmail.com>
2024-12-12 19:33:33 +0900

    過去のライブ映像とyoutubeリンクを一致させた

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,4 +1,8 @@
 class PagesController < ApplicationController
   def home
   end
+
+  def past_performances
+    # 過去のライブ映像ページ用の処理
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,9 @@ Rails.application.routes.draw do
 }
 
   get "/users/sign_out", to: "sessions#destroy", as: :get_logout
-
+  
   root "pages#home" # トップページを PagesController の home アクションに設定
+  get "/past_performances", to: "pages#past_performances", as: :past_performances
+
   resources :bands, only: [:new, :create, :index]
 end
